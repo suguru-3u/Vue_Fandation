97960c150ff2143eec6f3a5225821dd81047d2f9
Oohira Suguru <suguru.13579515@gmail.com>
2020-08-02 16:24:18 +0900

    study Vue.js

--- Vue.js
+++ Vue.js
@@ -1,6 +1,49 @@
-new Vue({
+// const vm =new Vue({
+//   data: {
+//   	message: 'Hello word'
+//   }
+// })
+//
+// vm.$mount('#app')
+// const vm = new Vue({
+// 	el: '#app',
+//   data(){
+//   	return {
+//     	message: 'Halleo'
+//     }
+//   }
+// })
+//
+// window.vm = vm
+//
+//  vm.$watch(function(){
+// 	 return this.message
+//  },function(message){
+// 	 console.log('変更されました!' + message)
+//  })
+const vm = new Vue({
 	el: '#app',
-  data: {
-  	message: 'Hello word'
-  }
+  data(){
+  	return {
+    	numbers: [],
+			total_num: 0
+		}
+   },
+	watch: {
+		numbers(value){
+			this.total_num = 0
+			this.numbers.forEach(number => {
+				this.total_num += number
+			})
+		}
+	}
 })
+
+
+window.vm = vm
+
+ // vm.$watch(function(){
+	//  return this.message
+ // },function(message){
+	//  console.log('変更されました!' + message)
+ // })
